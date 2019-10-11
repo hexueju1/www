4fcde9c1419b43735889511b4e41efeb14a602d3
playSCforever <471749824@qq.com>
2019-10-11 14:47:32 +0800

    upload to oss 提取

--- src/utils/MyPhotoSelectUtils.js
+++ src/utils/MyPhotoSelectUtils.js
@@ -1,4 +1,5 @@
 import ImagePicker from 'react-native-image-picker'
+import { showToast } from './MyToastUtils'
 
 /**
  *
@@ -20,7 +21,7 @@ const options = {
 }
 
 function handleResult(resolve, reject, response) {
-  console.log('Response = ', response)
+  // console.log('Response = ', response)
   if (response.didCancel) {
     console.log('User cancelled image picker')
   } else if (response.error) {
@@ -65,5 +66,84 @@ export function showImagePicker() {
     ImagePicker.showImagePicker(options, (response) => {
       handleResult(resolve, reject, response)
     })
+  })
+}
+
+// 图片上传
+export function uploadFileToOss(responseJson, targetUri, progressCallback, successCallback) {
+  let ossbase = responseJson.data.dir + 'authentication/idcard/' + responseJson.data.number + '_front.jpg'
+  let urlPath = responseJson.data.host + '/' + ossbase
+  console.log('target:' + urlPath)
+
+  const uploadMediaData = new FormData()
+  uploadMediaData.append('OSSAccessKeyId', responseJson.data.accessid)
+  uploadMediaData.append('policy', responseJson.data.policy)
+  uploadMediaData.append('Signature', responseJson.data.signature)
+  uploadMediaData.append('key', ossbase)
+  uploadMediaData.append('success_action_status', 200)
+  uploadMediaData.append('file', {
+    uri: targetUri,
+    type: 'multipart/form-data',
+    name: 'file',
+  })
+
+  //开始上传
+  const OSS_UPLOAD_URI = responseJson.data.host
+  doUpload(
+    OSS_UPLOAD_URI,
+    {
+      method: 'POST',
+      body: uploadMediaData,
+      extra: null,
+    },
+    (progressEvent) => {
+      const progress = progressEvent.loaded / progressEvent.total
+      console.log('progress = ' + progress)
+      progressCallback(progress)
+    },
+    (xhr) => {
+      showToast('图片上传成功')
+      successCallback(urlPath)
+    },
+    (xhr) => {
+      showToast('图片上传成功')
+    },
+  ).then(
+    (res) => {
+      // console.log(res)
+    },
+    (err) => showToast('图片上传失败'),
+  )
+}
+
+//这个方法就是具体上传的代码了
+function doUpload(url, opts = {}, onProgress, successResponse, failResponse) {
+  return new Promise((res, rej) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open(opts.method || 'get', url)
+    for (let k in opts.headers || {}) {
+      xhr.setRequestHeader(k, opts.headers[k])
+    }
+    xhr.onload = (e) => res(e)
+    xhr.onreadystatechange = (e) => {
+      console.log('onreadystatechange')
+      if (xhr.readyState !== 4) {
+        return
+      }
+      //阿里云的状态码200 才有返回的信息
+      if (xhr.status === 200) {
+        xhr.extra = opts.extra
+        successResponse(xhr)
+      } else {
+        xhr.extra = opts.extra
+        failResponse(xhr)
+      }
+    }
+    xhr.onerror = rej
+    if (xhr.upload && onProgress) {
+      xhr.upload.onprogress = onProgress
+    }
+    xhr.setRequestHeader('Content-Type', 'multipart/form-data')
+    xhr.send(opts.body)
   })
 }
