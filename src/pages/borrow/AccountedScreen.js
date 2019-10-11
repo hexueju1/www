64d1f4fa969579b6b0f3b439bd3e7b6b766cbfbc
yuyaku <yuyaku@163.com>
2019-10-11 14:02:37 +0800

    add AccountedScreen

--- src/pages/borrow/AccountedScreen.js
+++ src/pages/borrow/AccountedScreen.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react'
-import { ScrollView, StyleSheet, View, TouchableOpacity, Image } from 'react-native'
+import { ScrollView, StyleSheet, View, Dimensions, TouchableOpacity, Image, Linking, SettingItem } from 'react-native'
 import { Button, Text } from 'native-base'
 import BaseScreen from '../../components/BaseScreen'
 import { px, sp } from '../../utils/Device'
@@ -17,13 +17,44 @@ import { images } from '../../common/Constants'
 export default class AccountedScreen extends BaseScreen {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      money: '2,000.00',
+    }
   }
 
   render() {
+    let invoice = [
+      { id: 111, content: '完成申请', ctime: '2017-1-11 17:59' },
+      { id: 222, content: '申请成功，银行受理中', ctime: '2017-1-10 17:59' },
+      { id: 222, content: '借款成功', ctime: '2017-1-10 17:59' },
+    ]
+    let items = []
+    invoice.map((el, index) => {
+      let colorValue = index === 0 ? '#E7912D' : '#888'
+      let backgroundColor = index === 0 ? '#E7912D' : '#e0e0e0'
+      items.push(
+        <View style={styles.expressItem} key={index}>
+          <View style={styles.expressRightFirst}>
+            <View style={styles.process}>
+              <Text style={{ color: colorValue, fontSize: 14 }}>{el.content}</Text>
+              <Text style={{ color: colorValue, fontSize: 12 }}>{el.ctime}</Text>
+            </View>
+          </View>
+          <View style={[styles.expressLeft, { backgroundColor: backgroundColor }]} />
+        </View>,
+      )
+    })
     return (
       <View style={styles.main_container}>
         <TabHeader text="借款进度" />
+        <View style={styles.content}>
+          <Text style={{ color: '#666666', fontSize: sp(18), marginTop: px(10) }}>已到账</Text>
+          <Text style={{ color: '#0F0F0F', fontSize: sp(38), marginTop: px(16), fontWeight: '400' }}>{this.state.money}</Text>
+        </View>
+        <View style={styles.bottom}>
+          <Text style={{ color: '#ABABAB', fontSize: sp(14), margin: px(18) }}>交易进度</Text>
+          <View style={styles.step}>{items}</View>
+        </View>
       </View>
     )
   }
@@ -45,32 +76,48 @@ var styles = StyleSheet.create({
   },
   content: {
     alignItems: 'center',
-  },
-  box: {
-    width: px(338),
-    height: px(177),
+    height: px(128),
     backgroundColor: '#FDFDFD',
-    borderRadius: px(8),
-    marginTop: px(41),
-    paddingTop: px(15),
   },
-  row: {
+  bottom: {
+    marginTop: px(8),
+    height: px(185),
+    backgroundColor: '#FDFDFD',
     flexDirection: 'row',
-    marginBottom: px(10),
   },
-  list_item: {
-    marginLeft: px(5),
-    marginRight: px(5),
-    padding: px(5),
-    height: px(30),
+  step: {
+    marginLeft: 10,
+    flexDirection: 'column',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    marginTop: 10,
   },
-  list_item_title: {
-    flex: 1,
-    fontSize: sp(14),
-    color: '#ABABAB',
+  process: {
+    paddingVertical: 10,
+    flexDirection: 'column',
+    paddingRight: 20,
   },
-  list_item_desc: {
-    color: '#0F0F0F',
-    fontSize: sp(14),
+  expressRightFirst: {
+    width: Dimensions.get('window').width,
+    paddingLeft: 25,
+    borderLeftWidth: 1,
+    borderLeftColor: '#e0e0e0',
+    flexDirection: 'column',
+  },
+  expressItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingLeft: 10,
+    width: Dimensions.get('window').width,
+  },
+  expressLeft: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#e0e0e0',
+    position: 'relative',
+    right: Dimensions.get('window').width + 4,
+    top: 20,
   },
 })
