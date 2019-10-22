1a833d3cb7f68d61adda6cddb3f8fdea4888bfce
yuyaku <yuyaku@163.com>
2019-10-22 15:17:59 +0800

    还款界面--接口未提供

--- src/pages/repayment/RepaymentScreen.js
+++ src/pages/repayment/RepaymentScreen.js
@@ -28,6 +28,7 @@ export default class RepaymentScreen extends React.Component {
     super(props)
     this.state = {
       hasBorrowed: '2,000.00',
+      language:'全额还款'
     }
   }
 
@@ -40,57 +41,36 @@ export default class RepaymentScreen extends React.Component {
             this.props.navigation.goBack()
           }}
         />
-
-        {/* <ScrollView
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '85%',
-            paddingHorizontal: px(28),
-            marginTop: size.statusbar_height + px(30),
-          }}
+        <ScrollView
+          style={{ position: 'absolute', width: '100%', height: '85%', paddingHorizontal: px(28), marginTop: size.statusbar_height + px(30) }}
           contentContainerStyle={{}}
         >
-
           <ImageBackground style={[styles.main]} source={images.repay_main}>
-
             <Picker
               selectedValue={this.state.language}
-              style={[styles.select]}
+              style={styles.select}
               onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}
             >
               <Picker.Item value="pay_all" label="全额还款" />
-
               <Picker.Item value="oneday" label="续期一天" />
-
-              <Picker.Item value="period" label="续期一期" />
+              <Picker.Item value="period" label="续期一天" />
             </Picker>
-
-            <View
-              style={{
-                height: '100%',
-                position: 'absolute',
-                marginTop: px(38),
-                marginLeft: px(103),
-              }}
-            >
+            <View style={{ height: '100%', position: 'absolute', marginTop: px(38), marginLeft: px(103) }}>
               <Text style={{ color: '#0F0F0F', fontSize: sp(14) }}>2019年08月08日</Text>
               <Text style={{ color: '#0F0F0F', fontSize: sp(14) }}>15:30</Text>
             </View>
           </ImageBackground>
-
           <View style={{ position: 'relative' }}>
             <Text style={{ color: '#F0A00B', fontWeight: 'bold', fontSize: sp(44), position: 'absolute', top: px(-118), left: px(30) }}>
               ¥{this.state.hasBorrowed}
             </Text>
           </View>
-
           <View style={[styles.touchableopacity]}>
             <TouchableOpacity style={styles.button} onPress={() => {}}>
               <Text style={styles.buttonText}>{'立即支付'}</Text>
             </TouchableOpacity>
           </View>
-        </ScrollView> */}
+        </ScrollView>
       </View>
     )
   }
@@ -153,5 +133,7 @@ var styles = StyleSheet.create({
     top: px(38),
     backgroundColor: color.primary_bg,
     color: '#FDFDFD',
+
+    borderTopLeftRadius: px(100),
   },
 })
